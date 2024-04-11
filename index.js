496b732871f9aa1d844f37eb0af167543789ee73
Bucky TheWinterSoldier <nagendran.ashok@outlook.com>
2024-04-11 07:51:09 +0000

    File MetaService

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+let multer = require('multer');
+let upload=multer({dest:"uploads/"});
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +13,23 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',upload.single('upfile'),(request,response)=>{
+  console.log(request.file);
+ response.json({
+  "name": request.file.originalname,
+  "type": request.file.mimetype,
+  "size": request.file.size
+})
+});
+// app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+//   // setting json headers
+//   res.set({ 'Content-Type': 'application/json' });
+  
+//   // going on the logic :
+//   var file = req.file;
+//   if(file == undefined) res.json({error: "No file submitted"});
+//   else res.json({size: file.size, mimetype: file.mimetype, name: file.originalname});
+// });
 
 
 
